364fcb1cf38fe0c75a7b9600b5e4bbb4cf4bd0f6
huai0216 <52515300+huai0216@users.noreply.github.com>
2019-07-05 17:26:47 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,39 @@
 
 
 function start(e) {
+    var name = [
+    {
+        char:'王',
+        big5:'A4FD',
+        cns: '1-455E',
+        unicode:'738B'
+    },
+    {
+        char:'劭',
+        big5:'CA6F',
+        cns: '2-2331',
+        unicode:'52AD'
+    },
+    {
+        char:'懷',
+        big5:'C368',
+        cns: '1-7835',
+        unicode:'61F7'
+    }
+];
 
+    var ele = document.getElementById("div1");
+
+    for (var i = 0; i < 3; i++) {
+        ele.innerHTML += name[i].char;
+        ele.innerHTML += ' big5 = ';
+        ele.innerHTML += name[i].big5;
+        ele.innerHTML += ' cns =';
+        ele.innerHTML += name[i].cns;
+        ele.innerHTML += 'unicode =';
+        ele.innerHTML += name[i].unicode;
+        ele.innerHTML += '<br>';
+    }
 }
 
 /*
